648f9b594243a15d634593cc2b9af2901c7ba400
Murat <muratruslanov@gmail.com>
2017-11-11 15:20:19 +0600

    app rename

--- src/main/java/kg/kgiai/dekanat/rest_api/DekanatApplication.java
+++ src/main/java/kg/kgiai/dekanat/rest_api/DekanatApplication.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(value = "http://localhost:4200")
-public class TimetableApplication {
+public class DekanatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TimetableApplication.class, args);
+		SpringApplication.run(DekanatApplication.class, args);
 	}
 
 
